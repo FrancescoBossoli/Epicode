5082f74663cefc9b5f0d29a5c47e5da1c9ead66b
Francesco Bossoli <francescobossoli@hotmail.com>
2022-10-25 10:42:09 +0200

    fix

--- Settimana 04/Esercizio02/assets/js/script.js
+++ Settimana 04/Esercizio02/assets/js/script.js
@@ -1,16 +1,21 @@
 function spendi() {
     var budget = Number(document.getElementById('budgetIniziale').value);
-    var spese = 0;
-    var acquisto = 0;
-    var rimanenza = budget;
-    document.getElementById('scelta').innerHTML = "Il tuo budget iniziale è " + budget + "€";
-    for (spese = 0; rimanenza >= (budget/10); spese++) {
-        acquisto = Math.floor(Math.random() * budget/10);
-        rimanenza -= acquisto;
-        document.getElementById('lista').innerHTML += "<li>Ammontare della spesa: €" + acquisto + "<span>";
-        document.getElementById('lista').innerHTML += "Nuovo budget aggiornato: €" + rimanenza + "</span></li>";
+    if (budget > 100) {
+        var spese = 0;
+        var acquisto = 0;
+        var rimanenza = budget;
+        document.getElementById('scelta').innerHTML = "Il tuo budget iniziale è " + budget + "€";
+        for (spese = 0; rimanenza >= (budget / 10); spese++) {
+            acquisto = Math.floor(Math.random() * budget / 10);
+            rimanenza -= acquisto;
+            document.getElementById('lista').innerHTML += "<li>Ammontare della spesa: €" + acquisto + "<span>";
+            document.getElementById('lista').innerHTML += "Nuovo budget aggiornato: €" + rimanenza + "</span></li>";
         }
-    document.getElementById('lista').innerHTML += "<h2>Hai speso abbastanza</h2>";
-    document.getElementById('lista').innerHTML += "<p>Totale pese effettuate: " + spese + "</p>";
-    document.getElementById('lista').innerHTML += "<p>Budget rimanente: €" + rimanenza + "</p>";
+        document.getElementById('lista').innerHTML += "<h2>Hai speso abbastanza!</h2>";
+        document.getElementById('lista').innerHTML += "<p>Totale spese effettuate: " + spese + "</p>";
+        document.getElementById('lista').innerHTML += "<p>Budget rimanente: €" + rimanenza + "</p>";
+    }
+    else {
+        document.getElementById('scelta').innerHTML = "Hai un budget troppo basso per poterti dare a spese folli";
+    }
 }
