e3636e27ce614a4d3faffc350a81d35de778e391
Francesco Bossoli <francescobossoli@hotmail.com>
2022-10-25 10:50:43 +0200

    update

--- Settimana 04/Esercizio02/assets/js/script.js
+++ Settimana 04/Esercizio02/assets/js/script.js
@@ -11,9 +11,9 @@ function spendi() {
             document.getElementById('lista').innerHTML += "<li>Ammontare della spesa: €" + acquisto + "<span>";
             document.getElementById('lista').innerHTML += "Nuovo budget aggiornato: €" + rimanenza + "</span></li>";
         }
-        document.getElementById('lista').innerHTML += "<h2>Hai speso abbastanza!</h2>";
-        document.getElementById('lista').innerHTML += "<p>Totale spese effettuate: " + spese + "</p>";
-        document.getElementById('lista').innerHTML += "<p>Budget rimanente: €" + rimanenza + "</p>";
+        document.getElementById('esito').innerHTML += "<h2>Hai speso abbastanza!</h2>";
+        document.getElementById('esito').innerHTML += "Totale spese effettuate: " + spese + "<br>";
+        document.getElementById('esito').innerHTML += "Budget rimanente: €" + rimanenza;
     }
     else {
         document.getElementById('scelta').innerHTML = "Hai un budget troppo basso per poterti dare a spese folli";
